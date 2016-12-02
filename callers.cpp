609d99adcdd9a36c7df3be8da2704a718498b0ae
Haifeng Yang <hfyangpku@gmail.com>
2016-12-02 01:38:02 -0500

    Start to calculate at given radius.

--- callers.cpp
+++ callers.cpp
@@ -42,11 +42,39 @@ int call_HLTau(){
   string Fout=line;
   cout<<"Output file: "<<Fout<<endl;
 
+  // Seventh line is the calculation mode
+  int NMode;
+  Fpar>>NMode;
+  getline(Fpar, line);
+
   HLTau M = HLTau();
   M.set_kappa(Cabs+Csca, Csca);
   if (rho0!=-1) M.set_rho0(rho0);
   else cout<<"Using default density profile."<<endl;
-  M.get_Circle_Image(theta, 15, 16, 10*AU, 150*AU, Fout);
+
+  switch(NMode){
+  case 0:
+    cout<<"Doing HLTau calculation in Standard Mode with a grid of 15x16 points."<<endl;
+    M.get_Circle_Image(theta, 15, 16, 10*AU, 150*AU, Fout);
+    break;
+  case 1:
+    cout<<"Doing calculation at given radius."<<endl;
+    // Eighth line is Number of points in Radial direction
+    int nr, np;
+    Fpar>>nr; getline(Fpar, line);
+    Fpar>>np; getline(Fpar, line);
+    double R;
+    for (int i=0;i<nr;i++){
+      Fpar>>R;
+      cout<<"Calculating at radius: "<<R<<endl;
+    }
+  case 2:
+    cout<<"Restart Mode not implemented yet. I'll do nothing here for now."<<endl;
+    break;
+  default:
+    cout<<"ERROR: Calculation Mode "<<NMode<<" is not valid. please use 0 for standard mode, 1 for given radius and 2 for restart."<<endl;
+    return 3;
+  }
 
   return 0;
 }
